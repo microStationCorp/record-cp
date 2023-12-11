2aa5136c9945b236077cd5e1946a4a34511a3ffc
sujan mondal <sujanmondal916@gmail.com>
2023-12-11 14:03:29 +0530

    speedinsight integeration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <SpeedInsights/>
       </body>
     </html>
   );
